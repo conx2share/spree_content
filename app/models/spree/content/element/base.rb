ed48e2fd353a1da7d2e7e5c76d028a73e7582f32
Ivgeni Slabkovski <ivgeni@conx2share.com>
2016-02-25 14:00:20 -0500

    Fixes instance var naming syntax.

--- app/models/spree/content/element/base.rb
+++ app/models/spree/content/element/base.rb
@@ -41,12 +41,12 @@ module Spree::Content
         if template.present?
           options = options.dup
           options[:template] ||= template
-          old_element = context.instance_variable_get(:current_element) if context.instance_variable_defined?(:current_element)
-          context.assign({ current_element: self })
+          old_element = context.instance_variable_get(:@element) if context.instance_variable_defined?(:@element)
+          context.assign({ element: self })
           renderer.render(context, options)
         end
       ensure
-        context.assign({ current_element: old_element })
+        context.assign({ element: old_element })
       end
 
       private
